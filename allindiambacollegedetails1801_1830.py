ea0b5ecdc861acc914381fc4b33bc6c12eb98083
cvm8173-hub <cvm8173@gmail.com>
2026-03-16 11:42:35 +0530

    Update allindiambacollegedetails1801_1830.py

--- allindiambacollegedetails1801_1830.py
+++ allindiambacollegedetails1801_1830.py
@@ -9252,7 +9252,7 @@ def parse_faq_scholarships_section(driver, URLS):
 def scrape_mba_colleges():
     driver = create_driver()
     all_data = []
-    c_count =2211
+    c_count =751
 
     try:
         for base_url in BASE_URL:
